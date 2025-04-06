1e876a325b9b4e954a0dfe3208c0aa069c2d94a6
Louis <louis06072004@gmail.com>
2025-04-06 12:17:09 +0700

    Add singleton code base

---
+++ Assets/Scripts/Core/Singleton/MonoSingleton.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+
+namespace Main.Singleton
+{
+    /// <summary>
+    /// The basic MonoBehaviour singleton implementation, this singleton is destroyed after scene changes, use <see cref="PersistentMonoSingleton{T}"/> if you want a persistent and global singleton instance.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+    {
+        #region Fields
+
+        /// <summary>
+        /// The instance.
+        /// </summary>
+        private static T instance;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <value>The instance.</value>
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+#if UNITY_6000
+                    instance = FindAnyObjectByType<T>();
+#else
+                    instance = FindObjectOfType<T>();
+#endif
+                    if (instance == null)
+                    {
+                        GameObject obj = new GameObject();
+                        obj.name = typeof(T).Name;
+                        instance = obj.AddComponent<T>();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        #endregion
+
+        #region Unity Messages
+
+        /// <summary>
+        /// Use this for initialization.
+        /// </summary>
+        protected virtual void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this as T;
+            }
+            else
+            {
+
+                // Destory duplicates
+                if (Application.isPlaying)
+                {
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    DestroyImmediate(gameObject);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        #endregion
+
+        #region Public Methods
+
+        public virtual void OnSingletonClear() { }
+
+        public static void CreateInstance()
+        {
+            DestroyInstance();
+            instance = Instance;
+        }
+
+        public static void DestroyInstance()
+        {
+            if (instance == null)
+            {
+                return;
+            }
+
+            instance.OnSingletonClear();
+            instance = default(T);
+        }
+
+        #endregion
+    }
+}
